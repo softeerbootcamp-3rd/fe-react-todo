5148dd03565096a06effe80cf180972493f388c5
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-29 15:55:20 +0900

    feat: handle form submit
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/components/todo-container/index.tsx
+++ src/components/todo-container/index.tsx
@@ -1,23 +1,57 @@
-import { FormEventHandler } from "react"
+import { FormEventHandler, useState } from "react"
 import TodoForm from "../todo-form"
 import TodoList from "../todo-list"
 import styles from "./styles.module.css"
+import TodoItem from "../todo-item"
+
+interface TodoItem {
+  id: string
+  content: string
+  completed: boolean
+}
 
 export default function TodoContainer() {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    // Prevent the browser from reloading the page
-    e.preventDefault()
+  const [items, setItems] = useState<TodoItem[]>([])
 
-    // Read the form data
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
     const form = e.target as HTMLFormElement
     const formData = new FormData(form)
-    const content = formData.get("todo-input")
+    const content = formData.get("todo-input")?.toString()
+
+    if (!content?.trim()) return
+
+    const newItem = {
+      id: new Date().getTime().toString(),
+      content,
+      completed: false,
+    }
+    setItems((prev) => [newItem, ...prev])
+
+    form.reset()
   }
+
+  const handleDelete = (id: string) => {}
+
+  const handleSave = (id: string) => {}
 
   return (
     <div>
       <TodoForm onSubmit={handleSubmit} />
-      <TodoList></TodoList>
+      <TodoList>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              id={item.id}
+              completed={item.completed}
+              content={item.content}
+              onDelete={handleDelete}
+              onSave={handleSave}
+            />
+          )
+        })}
+      </TodoList>
     </div>
   )
 }
