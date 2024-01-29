daa861ec528eafa787e96905320a2df28246d497
sean2337 <sean2337@naver.com>
2024-01-29 20:19:18 +0900

    fix#2_git_conflict_solve

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,8 +3,6 @@ import Header from "../component/header/Header";
 import Margin from "../component/Margin/Margin";
 import AddForm from "../component/AddForm/AddForm";
 import TodoList from "../component/TodoList/TodoList";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { addItem } from "../API/addItem";
 import { deleteItem } from "../API/deleteItem";
 import { useEffect, useState } from "react";
@@ -13,22 +11,6 @@ import { completeItem } from "../API/completeItem";
 const TodoMainWrapper = styled.div`
   width: 100%;
   height: auto;
-=======
-=======
-import { addItem } from "../API/addItem";
-import { deleteTodo } from "../API/deleteTodo";
-import { useEffect, useState } from "react";
-<<<<<<< HEAD
->>>>>>> 1dbb780 (feat#4_add_button_handler)
-=======
-import { completeItem } from "../API/completeItem";
->>>>>>> 571e54f (feat#5_completeItem_handler)
-
-const TodoMainWrapper = styled.div`
-  width: 100%;
-  height: 500px;
-  background-color: yellow;
->>>>>>> 9c88a56 (feat#3_add_todolist_component)
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -53,7 +35,7 @@ const TodoArr = [
 ];
 
 const MainPage = () => {
-  const [plusTodoText, setPlusTodoText] = useState("");
+  const [plusTodoText] = useState("");
   const [todoArr, setTodoArr] = useState(TodoArr);
 
   return (
@@ -61,8 +43,6 @@ const MainPage = () => {
       <Margin height="30px" />
       <Header>My Todo App</Header>
       <Margin height="50px" />
-<<<<<<< HEAD
-<<<<<<< HEAD
       <AddForm
         plusTodoText={plusTodoText}
         setPlusTodoText={setPlusTodoText}
@@ -82,32 +62,6 @@ const MainPage = () => {
             completeItem(todoItemId);
           }}
         >
-=======
-      <AddForm></AddForm>
-      <Margin height="20px" />
-      {TodoArr.map(({ todoItemId, todoText, isCompleted }) => (
-        <TodoList key={todoItemId} isCompleted={isCompleted}>
->>>>>>> 9c88a56 (feat#3_add_todolist_component)
-=======
-      <AddForm
-        setPlusTodoText={setPlusTodoText}
-        addBtnFun={() => {
-          addItem(plusTodoText, todoArr, setTodoArr);
-        }}
-      ></AddForm>
-      <Margin height="20px" />
-      {todoArr.map(({ todoItemId, todoText, isCompleted }) => (
-        <TodoList
-          key={todoItemId}
-          isCompleted={isCompleted}
-          deleteItem={() => {
-            deleteItem(todoItemId, todoArr, setTodoArr);
-          }}
-          completeItem={() => {
-            completeItem(todoItemId);
-          }}
-        >
->>>>>>> 1dbb780 (feat#4_add_button_handler)
           {todoText}
         </TodoList>
       ))}
@@ -116,3 +70,4 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
