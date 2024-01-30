b0e3ad31ee3ed4b0358e82edff192fb71c124258
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-30 10:58:40 +0900

    feat: use reducer for state management
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/components/todo-container/index.tsx
+++ src/components/todo-container/index.tsx
@@ -1,12 +1,37 @@
-import { FormEventHandler, useState } from "react"
+import { FormEventHandler, useReducer } from "react"
 import TodoForm from "../todo-form"
 import TodoList from "../todo-list"
-import styles from "./styles.module.css"
 import { type Item } from "./types"
 import TodoItem from "../todo-item"
 
+type Action =
+  | { type: "ADD"; payload: { item: Item } }
+  | { type: "EDIT"; payload: { item: Item } }
+  | { type: "COMPLETED"; payload: { item: Item } }
+  | { type: "DELETE"; payload: { id: Item["id"] } }
+
+function todoReducer(state: Item[], action: Action) {
+  switch (action.type) {
+    case "ADD":
+      return [action.payload.item, ...state]
+    case "EDIT":
+    case "COMPLETED": {
+      const newItems = [...state]
+      const index = newItems.findIndex(
+        ({ id }) => action.payload.item.id === id
+      )
+      newItems.splice(index, 1, action.payload.item)
+      return newItems
+    }
+    case "DELETE":
+      return state.filter((item) => action.payload.id !== item.id)
+    default:
+      return state
+  }
+}
+
 export default function TodoContainer() {
-  const [items, setItems] = useState<Item[]>([])
+  const [items, dispatch] = useReducer(todoReducer, [])
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
@@ -21,22 +46,17 @@ export default function TodoContainer() {
       content,
       completed: false,
     }
-    setItems((prev) => [newItem, ...prev])
+    dispatch({ type: "ADD", payload: { item: newItem } })
 
     form.reset()
   }
 
   const handleDelete = (id: string) => {
-    setItems((prev) => prev.filter((item) => item.id !== id))
+    dispatch({ type: "DELETE", payload: { id } })
   }
 
   const handleSave = (newItem: Item) => {
-    setItems((prev) => {
-      const index = prev.findIndex(({ id }) => newItem.id === id)
-      const newItems = [...prev]
-      newItems.splice(index, 1, newItem)
-      return newItems
-    })
+    dispatch({ type: "EDIT", payload: { item: newItem } })
   }
 
   return (
